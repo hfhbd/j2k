7fd3c31575d5e833b8d2d619f90a41c18a7884f2
hfhbd <hfhbd@users.noreply.github.com>
2023-09-21 10:25:27 +0200

    Cleanup

--- j2k-core/src/main/kotlin/J2KConverter.kt
+++ j2k-core/src/main/kotlin/J2KConverter.kt
@@ -6,25 +6,20 @@ import com.intellij.lang.java.*
 import com.intellij.lang.java.parser.*
 import com.intellij.openapi.application.*
 import com.intellij.openapi.extensions.*
-import com.intellij.openapi.projectRoots.*
 import com.intellij.openapi.util.*
-import com.intellij.openapi.vfs.*
 import com.intellij.openapi.vfs.newvfs.*
 import com.intellij.openapi.vfs.newvfs.persistent.*
 import com.intellij.psi.*
-import com.intellij.psi.search.*
 import com.intellij.psi.stubs.*
 import com.intellij.psi.util.PsiUtil.*
 import com.intellij.util.indexing.*
 import org.jetbrains.kotlin.cli.jvm.compiler.*
 import org.jetbrains.kotlin.idea.*
-import org.jetbrains.kotlin.idea.core.script.*
 import org.jetbrains.kotlin.idea.stubindex.*
 import org.jetbrains.kotlin.j2k.*
 import org.jetbrains.kotlin.nj2k.*
 import org.jetbrains.kotlin.nj2k.NewJavaToKotlinConverter.Companion.addImports
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.scripting.resolve.*
 
 private class ApplicationEnvironment {
     val disposer = Disposer.newDisposable()
@@ -62,92 +57,8 @@ private class ApplicationEnvironment {
 
         ApplicationManager.setApplication(application, {
             CoreFileTypeRegistry().apply { registerFileType(JavaFileType.INSTANCE, "java") }
-        }, parentDisposable)/*
-        //
-*//*
-        registerApplicationService(ProjectManager::class.java, ProjectManagerExImpl())
-
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            MetaLanguage.EP_NAME,
-            MetaLanguage::class.java,
-        )
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            SmartPointerAnchorProvider.EP_NAME,
-            SmartPointerAnchorProvider::class.java,
-        )
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            AdditionalLibraryRootsProvider.EP_NAME,
-            AdditionalLibraryRootsProvider::class.java
-        )
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            KotlinBinaryExtension.EP_NAME, KotlinBinaryExtension::class.java
-        )
-
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            PsiAugmentProvider.EP_NAME, PsiAugmentProvider::class.java,
-        )
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            SuperMethodsSearch.EP_NAME,
-            com.intellij.util.QueryExecutor::class.java,
-        )
-
-        CoreApplicationEnvironment.registerApplicationExtensionPoint(
-            StubIndexExtension.EP_NAME,
-            StubIndexExtension::class.java
-        )
-        registerApplicationService(StubIndex::class.java, StubIndexImpl())
-
-        CoreApplicationEnvironment.registerApplicationDynamicExtensionPoint(
-            KotlinFullClassNameIndex.KEY.name,
-            KotlinFullClassNameIndex::class.java
-        )
-        addExtension(
-            ExtensionPointName(KotlinFullClassNameIndex.KEY.name),
-            KotlinFullClassNameIndex
-        )
-
-        registerApplicationService(
-            com.intellij.openapi.editor.EditorFactory::class.java, MockEditorFactory()
-        )
-        addExplicitExtension(LanguageASTFactory.INSTANCE, JavaLanguage.INSTANCE, JavaASTFactory())
-
-         */
+        }, parentDisposable)
     }
-}
-
-private object FakeScriptConfigurationManager : ScriptConfigurationManager {
-    override fun getAllScriptDependenciesSources(): Collection<VirtualFile> = emptyList()
-
-    override fun getAllScriptDependenciesSourcesScope(): GlobalSearchScope = GlobalSearchScope.EMPTY_SCOPE
-
-    override fun getAllScriptsDependenciesClassFiles(): Collection<VirtualFile> = emptyList()
-
-    override fun getAllScriptsDependenciesClassFilesScope(): GlobalSearchScope {
-        TODO("Not yet implemented")
-    }
-
-    override fun getConfiguration(file: KtFile): ScriptCompilationConfigurationWrapper? = null
-
-    override fun getFirstScriptsSdk(): Sdk? = null
-
-    @Deprecated("Use getScriptClasspath(KtFile) instead", ReplaceWith("emptyList()"))
-    override fun getScriptClasspath(file: VirtualFile): List<VirtualFile> = emptyList()
-
-    override fun getScriptClasspath(file: KtFile): List<VirtualFile> = emptyList()
-
-    override fun getScriptDependenciesClassFilesScope(file: VirtualFile): GlobalSearchScope {
-        TODO("Not yet implemented")
-    }
-
-    override fun getScriptSdk(file: VirtualFile): Sdk? = null
-
-    override fun hasConfiguration(file: KtFile): Boolean = false
-
-    override fun isConfigurationLoadingInProgress(file: KtFile): Boolean = false
-
-    override fun loadPlugins() {}
-
-    override fun updateScriptDefinitionReferences() {}
 }
 
 public class J2KConverter : AutoCloseable {
@@ -166,106 +77,7 @@ public class J2KConverter : AutoCloseable {
             ExtensionPoint.Kind.INTERFACE,
         )
         kotlinProjectEnv.environment.registerParserDefinition(JavaParserDefinition())
-        // LanguageParserDefinitions.INSTANCE.addExplicitExtension(KotlinLanguage.INSTANCE, KotlinParserDefinition())
 
-        /*
-        projectEnvironment.registerProjectComponent(
-            ProjectRootManager::class.java,
-            ProjectRootManagerImpl(projectEnvironment.project),
-        )
-
-        projectEnvironment.project.registerService(
-            ModuleManager::class.java, EmptyModuleManager(projectEnvironment.project)
-        )
-        projectEnvironment.project.registerService(
-            ProjectRootManager::class.java, ProjectRootManagerImpl(projectEnvironment.project)
-        )
-
-        projectEnvironment.project.registerService(
-            JavaFileManager::class.java,
-            CoreJavaFileManager(projectEnvironment.project.service())
-        )
-
-
-        projectEnvironment.project.extensionArea.registerExtensionPoint(
-            TreeGenerator.EP_NAME.name,
-            TreeGenerator::class.qualifiedName!!,
-            ExtensionPoint.Kind.INTERFACE
-        )
-        projectEnvironment.project.registerExtension(
-            TreeGenerator.EP_NAME,
-            JavaTreeGenerator(),
-            projectEnvironment.parentDisposable
-        )
-
-        projectEnvironment.project.extensionArea.registerExtensionPoint(
-            DirectoryIndexExcludePolicy.EP_NAME.name,
-            DirectoryIndexExcludePolicy::class.qualifiedName!!,
-            ExtensionPoint.Kind.INTERFACE
-        )
-        projectEnvironment.project.registerExtension(
-            DirectoryIndexExcludePolicy.EP_NAME,
-            object : DirectoryIndexExcludePolicy {},
-            projectEnvironment.parentDisposable
-        )
-
-        projectEnvironment.project.registerService(
-            PsiElementFactory::class.java,
-            PsiElementFactoryImpl(projectEnvironment.project)
-        )
-        projectEnvironment.project.registerService(
-            com.intellij.psi.JvmPsiConversionHelper::class.java,
-            JvmPsiConversionHelperImpl()
-        )
-        projectEnvironment.project.registerService(
-            com.intellij.psi.JavaPsiFacade::class.java,
-            JavaPsiFacadeImpl(projectEnvironment.project)
-        )
-
-        projectEnvironment.project.extensionArea.registerExtensionPoint(
-            KotlinBinaryExtension.EP_NAME.name,
-            KotlinBinaryExtension::class.qualifiedName!!,
-            ExtensionPoint.Kind.BEAN_CLASS
-        )
-        projectEnvironment.project.registerExtension(
-            KotlinBinaryExtension.EP_NAME, JavaClassBinary(), projectEnvironment.parentDisposable
-        )
-
-        projectEnvironment.project.extensionArea.registerExtensionPoint(
-            ScriptChangeListener.LISTENER.name,
-            ScriptChangeListener::class.qualifiedName!!,
-            ExtensionPoint.Kind.BEAN_CLASS
-        )
-        projectEnvironment.project.registerExtension(
-            ScriptChangeListener.LISTENER,
-            DefaultScriptChangeListener(projectEnvironment.project),
-            projectEnvironment.parentDisposable
-        )
-
-        projectEnvironment.project.registerService(KotlinPluginDisposable::class.java)
-
-        projectEnvironment.project.registerService(
-            ScriptConfigurationManager::class.java, FakeScriptConfigurationManager
-        )
-
-        projectEnvironment.project.registerService(
-            IDELanguageSettingsProviderHelper::class.java, IDELanguageSettingsProviderHelper(projectEnvironment.project)
-        )
-
-        projectEnvironment.project.registerService(
-            KotlinCommonCompilerArgumentsHolder::class.java,
-            KotlinCommonCompilerArgumentsHolder(projectEnvironment.project)
-        )
-
-        projectEnvironment.project.registerService(
-            com.intellij.psi.PsiResolveHelper::class.java,
-            PsiResolveHelperImpl(projectEnvironment.project)
-        )
-        projectEnvironment.project.registerService(
-            JavaResolveCache::class.java,
-            JavaResolveCache(projectEnvironment.project)
-        )
-*/
         converter = NewJavaToKotlinConverter(
             project = kotlinProjectEnv.project,
             targetModule = null,
