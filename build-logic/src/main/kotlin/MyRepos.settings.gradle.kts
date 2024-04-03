9cf42a6ba9efbd7b983d74d3d4df8af694d1fd2c
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-04-03 17:10:01 +0200

    Update dependencies (#43)
    * Bump gradle/gradle-build-action from 2 to 3
    
    Bumps [gradle/gradle-build-action](https://github.com/gradle/gradle-build-action) from 2 to 3.
    - [Release notes](https://github.com/gradle/gradle-build-action/releases)
    - [Commits](https://github.com/gradle/gradle-build-action/compare/v2...v3)
    
    ---
    updated-dependencies:
    - dependency-name: gradle/gradle-build-action
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update dependencies
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build-logic/src/main/kotlin/MyRepos.settings.gradle.kts
+++ build-logic/src/main/kotlin/MyRepos.settings.gradle.kts
@@ -5,7 +5,7 @@ dependencyResolutionManagement {
 
         maven(url = "https://www.jetbrains.com/intellij-repository/releases")
         maven(url = "https://cache-redirector.jetbrains.com/intellij-dependencies")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies/")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide/")
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
     }
 }
